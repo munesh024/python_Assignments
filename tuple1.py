93616e94e4419d3972ab967838f74eacce77fd58
Munesh J <68683398+munesh024@users.noreply.github.com>
2021-04-20 19:54:49 +0530

    Add files via upload
    Find all the index of 0 in the given tuple below.

---
+++ tuple1.py
@@ -0,0 +1,16 @@
+'''To find all the index of 0 in the given tuple below.
+tuple (0,1,2,3,0,1,3,0,23,0)'''
+
+
+
+count=0
+tup1 = (0,1,2,3,0,1,3,0,23,0)
+for each in tup1:
+    if each!=0:
+        count=count+1
+    
+    else :
+        print(tup1.index(each,count))
+        count=count+1
+        
+        
