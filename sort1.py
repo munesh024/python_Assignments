7240fcd1519c06987a6bb9d06408dce02451e48f
Munesh J <68683398+munesh024@users.noreply.github.com>
2021-04-21 15:56:41 +0530

    sort1.py
    Sorting the ip address depending on the last number

--- sort1.py
+++ sort1.py
@@ -1,11 +1,11 @@
 '''Sorting the ip address depending on the last number '''
 
 
-ip = ["192.168.0.09", "192.168.0.08","192.168.0.10", "192.168.0.15"]
+ip = ["192.168.0.09", "192.168.0.8","192.168.0.10", "192.168.0.15"]
 
 def fun(x):
     m = x.split(".")[3]
-    return m
+    return int(m)
     
 ip.sort(key=fun)
 print (ip)
