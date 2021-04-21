51bb41b2e85b9b97a947bb2f4421e9f3ef7be608
Munesh J <68683398+munesh024@users.noreply.github.com>
2021-04-21 07:14:11 +0530

    day1.py
    To check whether the given number is prime or not.

--- day1.py
+++ day1.py
@@ -5,7 +5,7 @@ m=int(input("enter the number"))
 count=0
 for i in range(1,m+1):  
     if m%i==0 :              #dividing given number from starting from 1 to n+1  
-        count=count+1        #if n+1 is divided by i then increase count by 1
+        count=count+1        #if m is divided by i then increase count by 1
         
 if count==2 :                #since prime number is divided by one and the number itself  ,if count is 2  
     print("prime number")    #it prints the number is prime or else not a prime
