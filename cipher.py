f9b567a8e0270d0ac12c1bf461b25e2c3ad21126
Munesh J <68683398+munesh024@users.noreply.github.com>
2021-04-29 11:42:52 +0530

    cipher1.py
    cipher program

---
+++ cipher.py
@@ -0,0 +1,31 @@
+#To Write the Caesar cipher program i.e. if we enter A then we have to get the 3rd element starting/ from A that is D.
+
+n=0
+str = input("enter the string")
+
+for each in str.upper():
+    if each=="X" :
+        n=1
+        m =ord("@")+n
+        m=chr(m)
+        print(m,end="")
+            
+        
+    elif each=="Y" :
+        n=2
+        m =ord("@")+n
+        m=chr(m)
+        print(m,end="")
+            
+            
+    elif each=="Z":
+        n=3
+        m =ord("@")+n
+        m=chr(m)
+        print(m,end="")
+            
+    else:
+        m=ord(each)+3
+        m=chr(m)
+        print(m,end="")
+
