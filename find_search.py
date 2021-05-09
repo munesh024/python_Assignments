93bcdb20bb42ebb66ef54b56326c5e4bfaf02077
Munesh J <68683398+munesh024@users.noreply.github.com>
2021-05-09 08:29:27 +0530

    find_search.py
    Use find method and find the occurrences of given Sub String in the given String.

---
+++ find_search.py
@@ -0,0 +1,27 @@
+'''Use find method and find the occurrences of given Sub String in the given String.
+● String = "A new Apple Insider report has suggested that the Apple iPhone series that will
+come out in 2022, likely the Apple iPhone 14 lineup,"
+● Sub String "Apple" '''
+
+
+st=""
+n=1
+count=0
+str1 = "A new Apple Insider report has suggested that the Apple iPhone series that will\
+come out in 2022, likely the Apple iPhone 14 lineup"
+for each in str1:
+    if each !=" " :
+        st=st+each
+        
+    else :
+        st=st+" "
+        m=st.find("Apple",n)
+        if m!= -1 :
+            count=count+1
+            n=n+m
+            
+        
+    
+        
+print(f"the count of the Apple is {count}")
+        
