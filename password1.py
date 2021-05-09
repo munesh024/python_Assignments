5ec9d52b8d551e483b645fbe9234a72a378ccda9
Munesh J <68683398+munesh024@users.noreply.github.com>
2021-05-09 08:42:50 +0530

    password1.py
    Using a random module generates a password satisfying the below conditions.
    Nature of the password: complex
    The password should contains
    ● 4 upper case
    ● 4 lowercase
    ● 2 special characters
    ● 2 digit

---
+++ password1.py
@@ -0,0 +1,21 @@
+import random 
+def password_gen(): 
+    str1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    str2 = "abcdefghijklmnopqrstuvwxyz"
+    str3 = "0123456789"
+    str4="~!@#$%^&*"
+    list1 = random.sample( str1,4) 
+    #print(list1)
+    list2 = random.sample( str2,4) 
+    #print(list2)
+    list3 = random.sample( str3,4)
+    list4 = random.sample( str4,4)
+    list1. extend( list2)
+    list1. extend( list3)
+    list1.extend(list4)
+    #print(list1)
+    random.shuffle( list1) 
+    otp = "". join( list1) 
+    print(otp)
+    
+password_gen()
